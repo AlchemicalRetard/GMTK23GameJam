da033deff723872368c3dd2ebf9615ee86088a39
Shahbaaz Khan <51862748+TheAwesomeShaz@users.noreply.github.com>
2023-07-09 19:26:58 +0530

    Chicken Sprites Added

--- GMTKJam23/Assets/Scripts/GameManager.cs
+++ GMTKJam23/Assets/Scripts/GameManager.cs
@@ -1,42 +1,131 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager instance;
+    public static GameManager Instance;
 
-    [SerializeField] int rageMultiplier;
-    [SerializeField] int waveNumber;
-    int currentRage;
-    int maxRage;
+    [SerializeField] int rageScoreMultiplier;
 
-    bool isPlayerDead;
-    bool isInRage;
-    int score;
+    [SerializeField] bool isPlayerDead;
+    [SerializeField] int score;
+
+    
+    public bool isInRage;
+    [SerializeField] float currentRage;
+    [SerializeField] int maxRage = 1000;
+    [SerializeField] private bool canRage;
+    [SerializeField] Slider rageSlider;
+
+    [SerializeField] int rageWhenKill = 100;
+    [SerializeField] int rageWhenHit = 300;
+    [SerializeField] int rageWhenEggsplosion = 200;
+
+    int[] rageArray = new int[2];
+
+    private PlayerMovement playerMovement;
+    private PlayerShooting playerShooting;
 
     private void Awake()
     {
-        instance = this;
+        // If there is an instance, and it's not me, delete myself.
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    void Start()
+    {
+        rageArray = new int[] {rageWhenHit,rageWhenKill};
     }
 
     public void AddScore(int value)
     {
         if (isInRage)
         {
-            score += value * rageMultiplier;
+            score += value * rageScoreMultiplier;
         }
         else
         {
             score += value;
         }
+
+        if (PlayerPrefs.GetInt("highScore") < score)
+        {
+            PlayerPrefs.SetInt("highScore", score);
+        }
     }
 
-    public void AddRage(int value)
+    public void AddRage(RageType rageType)
     {
         if (currentRage < maxRage)
         {
-            currentRage += value;
+            switch (rageType)
+            {
+                case RageType.GetHit:
+                    currentRage += rageArray[(int)RageType.GetHit];
+                    break;
+                case RageType.KillEnemy:
+                    currentRage += rageArray[(int)RageType.KillEnemy];
+                    break;
+                case RageType.Explosion:
+                    currentRage += rageArray[(int)RageType.Explosion];
+                    break;
+            }
+            rageSlider.value = currentRage;
+        }
+        if (currentRage >= maxRage)
+        {
+            canRage = true;
+            return;
         }
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q) && canRage)
+        {
+            isInRage = true;
+            
+            playerMovement.StartRage();
+            playerShooting.StartRage();
+
+            canRage = false;
+        }
+        ConsumeRage();
+    }
+
+    private void ConsumeRage()
+    {
+        if (isInRage)
+        {
+
+            // start Rage
+            currentRage -= Time.deltaTime;
+            rageSlider.value = currentRage;
+
+            if (currentRage <= 0)
+            {
+                currentRage = 0;
+                GameManager.Instance.isInRage = false;
+                playerMovement.EndRage();
+                playerShooting.EndRage();
+            }
+        }
+    }
+}
+public enum RageType
+{
+    GetHit, //0
+    KillEnemy, //0
+    Explosion, //0
 }
