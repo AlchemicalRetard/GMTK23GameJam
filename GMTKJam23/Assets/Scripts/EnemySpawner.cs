ab2f10f31dac9e8191e7a690cbf7f3829d682df7
AlchemicalRetard <devtherj@gmail.com>
2023-07-09 18:57:26 +0530

    boom wave manager

---
+++ GMTKJam23/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,37 @@
+using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public GameObject[] enemyPrefabs;
+    public SpawnPointManager spawnPointManager;
+    public int numberOfWaves = 5;
+    private float spawnInterval = 5.0f;
+    private int currentWave = 1;
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        while (currentWave <= numberOfWaves)
+        {
+            foreach (var spawnPoint in spawnPointManager.spawnPoints)
+            {
+                // choose a random enemy type to spawn
+                GameObject enemyPrefab = enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];
+
+                var enemy =  Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation).GetComponent<AIDestinationSetter>();
+                enemy.target = FindObjectOfType<PlayerMovement>().transform;
+                yield return new WaitForSeconds(spawnInterval);
+            }
+            currentWave++;
+            spawnInterval *= 0.9f; // decrease the spawn interval by 10% with each wave
+        }
+    }
+}
